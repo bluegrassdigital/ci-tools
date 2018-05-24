cfa13376290ff82a8d9717d8858187a7d752df65
Simon Buerger <simon@bluegrassdigital.com>
2018-05-24 21:07:16 +0200

    update path

--- bin/deploy-ios.js
+++ bin/deploy-ios.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const { exec, root, iosPaths } = require('.../lib/shared');
+const { exec, root, iosPaths } = require('../lib/shared');
 
 const { BINARY_PATH, SCHEME } = iosPaths;
 
