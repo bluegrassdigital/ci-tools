7b1b100c35a4f01766dda661adab69f4b974e4fa
Simon Buerger <simon@bluegrassdigital.com>
2018-05-25 13:06:17 +0200

    add base version env variable

--- bin/bump.js
+++ bin/bump.js
@@ -7,9 +7,11 @@ const pkg = require(path.resolve(process.cwd(), './package.json'));
 const { root } = require('../lib/shared');
 const { parseSemVer } = require('semver-parser');
 
-const version = parseSemVer(pkg.version);
+const { BUILD_NUMBER, APP_NAME, BASE_VERSION } = process.env;
 
-const { BUILD_NUMBER, APP_NAME } = process.env;
+const useVersion = BASE_VERSION || pkg.version;
+
+const version = parseSemVer(BASE_VERSION || pkg.version);
 
 const getAndroidVersionCode = v => {
   const {
@@ -20,18 +22,18 @@ const getAndroidVersionCode = v => {
 };
 
 const obj = plist.parse(fs.readFileSync(`${root}/ios/${APP_NAME || pkg.name}/Info.plist`, 'utf8'));
-obj.CFBundleVersion = `${pkg.version}.${BUILD_NUMBER}`;
-obj.CFBundleShortVersionString = pkg.version;
+obj.CFBundleVersion = `${useVersion}.${BUILD_NUMBER}`;
+obj.CFBundleShortVersionString = useVersion;
 fs.writeFileSync(`${root}/ios/${APP_NAME || pkg.name}/Info.plist`, plist.build(obj));
 
 const gradlePath = `${root}/android/app/build.gradle`;
 
 const gradle = fs.readFileSync(gradlePath, 'utf8');
-let str = gradle.replace(/versionName "(.*)"/, `versionName "${pkg.version}"`);
+let str = gradle.replace(/versionName "(.*)"/, `versionName "${useVersion}"`);
 str = str.replace(/versionCode (.*)/, `versionCode ${getAndroidVersionCode(version)}`);
 fs.writeFileSync(gradlePath, str);
 
-fs.writeFileSync(`${root}/version.properties`, `NEW_BUILD_VERSION=${pkg.version}.${BUILD_NUMBER}
-PKG_VERSION=${pkg.version}
+fs.writeFileSync(`${root}/version.properties`, `NEW_BUILD_VERSION=${useVersion}.${BUILD_NUMBER}
+PKG_VERSION=${useVersion}
 `);
 
