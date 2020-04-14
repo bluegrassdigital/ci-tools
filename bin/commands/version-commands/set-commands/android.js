fb362525c5a7e695234115cf0d96398e8ac7aed3
Simon Buerger <simon@bluegrassdigital.com>
2020-04-14 09:30:05 +0200

    Update to simple version setter

--- bin/commands/version-commands/set-commands/android.js
+++ bin/commands/version-commands/set-commands/android.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 const setVersion = require('../../../../lib/android/setVersion');
+const setVersionSimple = require('../../../../lib/android/setVersionSimple');
 
 exports.command = 'android'
 exports.desc = 'Set android version'
@@ -9,8 +10,13 @@ exports.builder = function (yargs) {
     .describe('gradlePath', 'Path of your main application build.gradle')
     .default('gradlePath', path.resolve(process.cwd(), './android/app/build.gradle'))
     .normalize('gradlePath')
+    .describe('simple', 'Use simple versioning to calculate android version code')
+    .boolean('simple')
+    .describe('startingVersionCode', 'Any calculated version code is added to this. Useful when switching from an old versioning system')
+    .number('startingVersionCode')
+    .default('startingVersionCode', 0)
     .global('v');
 }
 exports.handler = function (argv) {
-  return setVersion(argv);
+  return argv.simple ? setVersionSimple(argv) : setVersion(argv);
 }
